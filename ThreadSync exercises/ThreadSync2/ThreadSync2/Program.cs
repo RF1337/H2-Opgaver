c587ce988b1c0ec783b4a1566f7fdd96f675ad07
rf1337 <rasmus.ferst@gmail.com>
2024-01-25 10:45:25 +0100

    ThreadSync exercise 2

---
+++ ThreadSync exercises/ThreadSync2/ThreadSync2/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace ThreadSync2
+{
+    internal class Program
+    {
+        static int SymbolCount = 0;
+        object _lock = new object();
+
+        // Method for printing *
+        public void PrintStars()
+        {
+            while (true)
+            {
+                Monitor.Enter(_lock);
+                try
+                {
+                    for (int i = 0; i < 60; i++)
+                    {
+                        Console.Write("*");
+                        SymbolCount++;
+                        Thread.Sleep(1);
+                    }
+                    Console.Write(SymbolCount);
+                    Console.WriteLine();
+                }
+                finally
+                {
+                    Monitor.Exit(_lock);
+                }
+            }
+        }
+
+        public void PrintHashtags()
+        {
+            while (true)
+            {
+                Monitor.Enter(_lock);
+                try
+                {
+                    for (int i = 0; i < 60; i++)
+                    {
+                        Console.Write("#");
+                        SymbolCount++;
+                        Thread.Sleep(1);
+                    }
+                    Console.Write(SymbolCount);
+                    Console.WriteLine();
+                }
+                finally
+                {
+                    Monitor.Exit(_lock);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+
+            Thread thread = new Thread(pr.PrintStars);
+            Thread thread1 = new Thread(pr.PrintHashtags);
+
+            thread.Start();
+            thread1.Start();
+        }
+    }
+}
