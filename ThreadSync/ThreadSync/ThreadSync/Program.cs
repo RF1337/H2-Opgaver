0f68cd676af7d0e1cf157ffb3872364ea58d518b
rf1337 <rasmus.ferst@gmail.com>
2024-01-25 10:34:57 +0100

    ThreadSync exercises

---
+++ ThreadSync/ThreadSync/ThreadSync/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ThreadSync
+{
+    internal class Program
+    {
+        static int sum = 0;
+
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[1000];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(AddOne);
+                threads[i].Start();
+            }
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Join();
+            }
+
+            Console.WriteLine(sum);
+        }
+        static void AddOne()
+        {
+            //sum++;
+            Interlocked.Increment(ref sum);
+        }
+    }
+}
---
+++ ThreadSync/ThreadSync1/ThreadSync1/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ThreadSync1
+{
+    internal class Program
+    {
+        static int count = 0;
+        object _lock = new object();
+
+        public void CountUp()
+        {
+            while (true)
+            {
+                Monitor.Enter(_lock);
+                try
+                {
+                    count += 2;
+                    Console.Write(count + " ");
+                    Thread.Sleep(1000);
+                }
+                finally
+                {
+                    Monitor.Exit(_lock);
+                }
+            }
+        }
+
+        public void CountDown()
+        {
+            while (true)
+            {
+                Monitor.Enter(_lock);
+                try
+                {
+                    count--;
+                    Console.Write(count + " ");
+                    Thread.Sleep(1000);
+                }
+                finally
+                {
+                    Monitor.Exit(_lock);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+
+            Thread thread = new Thread(pr.CountUp);
+            Thread thread1 = new Thread(pr.CountDown);
+
+            thread.Start();
+            Thread.Sleep(1);
+            thread1.Start();
+        }
+    }
+}
