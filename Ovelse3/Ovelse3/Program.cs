0636f30173fc59c1876f0e9e04e6d4a9890c28fc
rf1337 <rasmus.ferst@gmail.com>
2024-01-25 10:24:43 +0100

    Basic exercise 3

---
+++ Ovelse3/Ovelse3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Ovelse3
+{
+    internal class Program
+    {
+        Random r = new Random();
+
+        static byte WrongNumber = 0;
+      
+        public void GenerateTemperature()
+        {
+            
+            while (WrongNumber < 3)
+            {
+               int randonNumber = r.Next(-20, 120);
+                Console.WriteLine($"{randonNumber}");
+                Thread.Sleep(100);
+
+                if (randonNumber > 100 || randonNumber < 0)
+                {
+                    WrongNumber++;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            Thread thread = new Thread(pr.GenerateTemperature);
+
+            // If it has printed wrong number 3 times then stop thread
+           
+            thread.Start();
+
+            Console.Read();
+        }
+    }
+}
