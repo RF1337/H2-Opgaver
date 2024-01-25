eb9596c386daa02add4a3f74bfe3ca0cb7d8747c
rf1337 <rasmus.ferst@gmail.com>
2024-01-25 10:17:14 +0100

    Basic exercise 1 & 2

---
+++ Ovelse1og2/Ovelse1og2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Ovelse1og2
+{
+    internal class Program
+    {
+        public void PrintToConsole1()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("C#-trådning er nemt");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void PrintToConsole2()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Også med flere tråde");
+                Thread.Sleep(1000);
+            }
+        }
+
+        class threprog
+        {
+            static void Main(string[] args)
+            {
+                Program pg = new Program();
+                
+                Thread thread = new Thread(pg.PrintToConsole1);
+                Thread thread2 = new Thread(pg.PrintToConsole2);
+                thread.Start();
+                thread2.Start();
+            }
+        }
+    }
+}
