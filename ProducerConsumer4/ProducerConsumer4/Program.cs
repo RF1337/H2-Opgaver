7c47c7cf4650d31c47cf8c537d2816fef142d6e0
rf1337 <rasmus.ferst@gmail.com>
2024-01-26 13:35:10 +0100

    Producer Consumer

---
+++ ProducerConsumer4/ProducerConsumer4/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ProducerConsumer4
+{
+    internal class Program
+    {
+        static Queue<int> products = new Queue<int>();
+
+        public void Produce()
+        {
+            int count = 0;
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+                while (products.Count < 3)
+                {
+                    count++;
+                    products.Enqueue(count);
+                    Console.WriteLine("Producer har produceret: " + products.Count);
+                }
+                if (products.Count == 3)
+                {
+                    Console.WriteLine("Kan ikke producere mere");
+                }
+            }
+        }
+
+        public void Consume()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                while (products.Count > 0)
+                {
+                    Console.WriteLine("Consumer har consumet " + products.Count);
+                    products.Dequeue();
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+
+            Thread producer = new Thread(pr.Produce);
+            producer.Start();
+
+            Thread consumer = new Thread(pr.Consume);
+            consumer.Start();
+
+            Console.Read();
+        }
+    }
+}
